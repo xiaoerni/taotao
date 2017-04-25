cf88b4dfb1d8a0433fe9f9906be1f99479990a0e
xiaoerni <xiao18333612616@163.com>
2017-04-25 15:02:53 +0800

    create project

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.taotao.service;
+
+import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.pojo.TbItem;
+
+public interface ItemService {
+
+	//根据itemId查询信息
+	TbItem getItemById(Long itemId);
+	
+	//进行分页查询
+	EasyUIDataGridResult getItemList(int page,int rows);
+}
