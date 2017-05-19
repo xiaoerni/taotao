68e1315cf91994e352ba84cfb1e0da3df9c080bb
xiaoerni <xiao18333612616@163.com>
2017-05-19 19:58:23 +0800

    购物车操作部分

---
+++ taotao-portal/src/main/java/com/taotao/portal/service/UserService.java
@@ -0,0 +1,11 @@
+package com.taotao.portal.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.taotao.pojo.TbUser;
+
+public interface UserService {
+
+	TbUser getUserByToken(HttpServletRequest request, HttpServletResponse response);
+}
